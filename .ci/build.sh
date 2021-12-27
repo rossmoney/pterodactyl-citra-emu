4eb11b49b0a00799bc73912d4023af8ab531ca8f
liushuyu <liushuyu011@gmail.com>
2021-12-26 19:30:33 -0700

    CI: add caching

--- .ci/build.sh
+++ .ci/build.sh
@@ -11,11 +11,18 @@ CFLAGS="-ftree-vectorize -flto"
 if [[ "$(uname -m)" == "aarch64" ]]; then
     CFLAGS="$CFLAGS -march=armv8-a+crc+crypto"
 fi
+if [[ "$USE_CCACHE" != '0' ]]; then
+    echo '[+] Enabled ccache'
+    ccache -s
+    export CC='/usr/lib/ccache/gcc'
+    export CXX='/usr/lib/ccache/g++'
+fi
+
 export CFLAGS
 export CXXFLAGS="$CFLAGS"
 export LDFLAGS="-flto -fuse-linker-plugin"
 
 mkdir -p build && cd build
-cmake .. -GNinja -DCMAKE_BUILD_TYPE=Release -DENABLE_QT=OFF -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=OFF -DUSE_DISCORD_PRESENCE=OFF -DENABLE_FFMPEG_VIDEO_DUMPER=OFF
+cmake .. -GNinja -DCMAKE_BUILD_TYPE=Release -DENABLE_SDL2=OFF -DENABLE_QT=OFF -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=OFF -DUSE_DISCORD_PRESENCE=OFF -DENABLE_FFMPEG_VIDEO_DUMPER=OFF
 ninja citra-room
 strip ./bin/Release/citra-room
--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,14 @@
+# syntax=docker/dockerfile:1.3
 FROM debian:bullseye AS build
 ENV DEBIAN_FRONTEND=noninteractive
+ARG USE_CCACHE
 RUN apt-get update && apt-get -y full-upgrade && \
-    apt-get install -y build-essential libsdl2-dev wget git ccache cmake ninja-build
+    apt-get install -y build-essential wget git ccache cmake ninja-build
 
 COPY . /root/build-files
 
-RUN git clone --recursive https://github.com/citra-emu/citra-canary.git /root/citra-canary && \
+RUN --mount=type=cache,id=ccache,target=/root/.ccache \
+    git clone --depth 1000 --recursive https://github.com/citra-emu/citra-canary.git /root/citra-canary && \
     cd /root/citra-canary && /root/build-files/.ci/build.sh
 
 FROM debian:bullseye-slim AS final
