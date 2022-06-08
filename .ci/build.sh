947c145a3226908a89cb1eef6652f91e137427ba
liushuyu <liushuyu011@gmail.com>
2022-06-07 19:19:16 -0600

    build: increase x86_64 image CPU baseline to Intel Core 2

--- .ci/build.sh
+++ .ci/build.sh
@@ -9,6 +9,9 @@ git am "$TOPDIR"/../patches/*.patch
 CFLAGS="-ftree-vectorize -flto"
 if [[ "$(uname -m)" == "aarch64" ]]; then
     CFLAGS="$CFLAGS -march=armv8-a+crc+crypto"
+elif [[ "$(uname -m)" == "x86_64" ]]; then
+    # those three of you still using Prescott should just compile this yourselves
+    CFLAGS="$CFLAGS -march=core2 -mtune=intel"
 fi
 if [[ "$USE_CCACHE" != '0' ]]; then
     echo '[+] Enabled ccache'
@@ -19,7 +22,7 @@ fi
 
 export CFLAGS
 export CXXFLAGS="$CFLAGS"
-export LDFLAGS="-flto -fuse-linker-plugin"
+export LDFLAGS="-flto -fuse-linker-plugin -fuse-ld=gold"
 
 mkdir -p build && cd build
 cmake .. -GNinja -DCMAKE_BUILD_TYPE=Release -DENABLE_SDL2=OFF -DENABLE_QT=OFF -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=OFF -DUSE_DISCORD_PRESENCE=OFF -DENABLE_FFMPEG_VIDEO_DUMPER=OFF
